b1f6a394a91476b1c3856ef78fb17a2c2b22844b
Jayson <ruirain16@gmail.com>
2016-10-13 09:27:53 +0800

    * order page header * monitoring pages

---
+++ Gentran/App/Monitoring/ViewModels/POMonitoring/MonitoringListModel.js
@@ -0,0 +1,29 @@
+﻿monitoringModule.controller("monitoringViewModel", function ($scope, monitoringService, $http, $q, $routeParams, $window, $location, viewModelHelper, DTOptionsBuilder, DTColumnDefBuilder) {
+
+    $scope.viewModelHelper = viewModelHelper;
+    $scope.monitoringService = monitoringService;
+
+    var initialize = function () {
+        //$scope.refreshCust();
+    }
+
+    //$scope.refreshCust = function () {
+    //    $scope.search = {};
+    //    $scope.searchBy = "CMCode";
+
+    //    viewModelHelper.apiGet('api/cust', null, function (result) {
+    //        $scope.customers = result.data.detail;
+    //    });
+
+    //    $scope.dtOptions = DTOptionsBuilder.newOptions();
+    //    $scope.dtColumnDefs = [
+    //       DTColumnDefBuilder.newColumnDef('no-sort').notSortable()
+    //    ];
+    //}
+
+    //$scope.addCustomer = function () {
+    //    $('#AddCustomerModal').modal('show');
+    //}
+
+    initialize();
+});
