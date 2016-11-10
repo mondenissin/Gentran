4716edae2a5efc49cbab36909aeeff0ac87628f6
Maurace <maurace.silva@gmail.com>
2016-11-10 15:25:08 +0800

    Admin: Dashboard

--- Gentran/App/Admin/ViewModels/Dashboard/DashboardModel.js
+++ Gentran/App/Admin/ViewModels/Dashboard/DashboardModel.js
@@ -3,17 +3,91 @@
     $scope.viewModelHelper = viewModelHelper;
     $scope.adminService = adminService;
 
-    $scope.totalOrders = 9999;
-    $scope.totalSKUs = 9999;
-    $scope.totalCustomers = 9999;
-    $scope.totalIncome = $scope.totalCustomers.toLocaleString();
+    //========
+    $scope.totalOrders = 0;
+    $scope.totalSKUs = 0;
+    $scope.totalQuantity = 0;
+    $scope.totalIncome = 0;
+
+    $scope.filterText = "";
+    //========
 
     var initialize = function () {
-        $scope.refreshDash();
+        $scope.getAccounts();
+
+        $scope.Items = null;
+        $scope.filterText = "(This year)";
+        $scope.refreshDash($scope.Items);
     }
 
-    $scope.refreshDash = function () {
-        
+    $scope.getAccounts = function () {
+        $scope.data = {};
+        $scope.data.operation = 'get_accounts';
+        viewModelHelper.apiGet('api/cust', $scope.data, function (result) {
+            $scope.accounts = result.data.detail;
+        });
+    }
+
+    $scope.refreshData = function (Items,callback) {
+        viewModelHelper.apiGet('api/dash', Items, function (result) {
+            var data = result.data;
+            var orders = 0, skus = 0, tskus = 0, amount = 0;
+            if (data.success == true) {
+                for (var i = 0; i < data.detail.length; i++) {
+                    orders += data.detail[i].ULTotalOrders;
+                    skus += data.detail[i].ULTotalSKUs;
+                    tskus += data.detail[i].ULTotalQuantity;
+                    amount += data.detail[i].ULTotalAmount;
+                }
+                callback(data);
+            }
+
+            $scope.totalOrders = orders;
+            $scope.totalSKUs = skus;
+            $scope.totalQuantity = tskus;
+            $scope.totalIncome = amount.toLocaleString();
+        });
+    }
+
+    $scope.setFilter = function () {
+        var filterArea = $('.ddl_area').val();
+        var filterChain = $('.ddl_chain').val();
+        var filterDate = $('.txt_daterange').val().split('-');
+
+        var operation = filterArea != '-1' && filterChain != '-1' ? "filter_dashboard_by_all" ? filterArea != '-1' : "filter_dashboard_by_area" : filterChain != '-1' ? "filter_dashboard_by_chain" : "filter_dashboard_date";
+
+        var dFrom = filterDate[0].split('/');
+        var dTo = filterDate[1].split('/');
+
+        var sFrom = dFrom[1] + '-' + dFrom[2] + '-' + dFrom[0];
+        var sTo = dTo[1] + '-' + dTo[2] + '-' + dTo[0];
+        var Item = {};
+
+        Item.dateFrom = sFrom + ' 00:00:00.000';
+        Item.dateTo = sTo + ' 23:59:59.999';
+
+        $scope.Items = {};
+        $scope.Items.operation = "filter_dashboard_date";
+        $scope.Items.payload = _payloadParser(Item);
+
+        $scope.filterText = sFrom == sTo ? 'Filter: ' + sTo : 'Filter: ' + sFrom + '  to ' + sTo;
+
+        $scope.refreshData($scope.Items, function (data) {
+            var areaData = [];
+            for (var i = 0; i < data.detail.length; i++) {
+                areaData[i] = {};
+                areaData[i].label = data.detail[i].RFRetrieveDate;
+                areaData[i].orders = data.detail[i].ULTotalOrders;
+                areaData[i].amount = data.detail[i].ULTotalAmount;
+            }
+            $scope.area.setData(areaData);
+        });
+    }
+
+    $scope.refreshDash = function (Items) {
+        $scope.refreshData(Items, function (data) {
+            $scope.AreaChartData(data.detail);
+        });
     }
    
     $scope.tools = function (proc, $event) {
@@ -35,5 +109,32 @@
             }
         }
     }
+
+    //Area Chart
+    $scope.AreaChartData = function (data) {
+        var areaData = [];
+        for (var i = 0; i < data.length; i++) {
+            areaData[i] = {};
+            areaData[i].label = data[i].RFRetrieveDate;
+            areaData[i].orders = data[i].ULTotalOrders;
+            areaData[i].amount = data[i].ULTotalAmount;
+        }
+        $scope.area = new Morris.Area({
+            element: 'revenue-chart',
+            resize: true,
+            data: areaData,
+            xkey: 'label',
+            parseTime: false,
+            ykeys: ['amount'],
+            yLabelFormat: function (y) { return 'P ' + y.toString(); },
+            xLabelFormat: function (x) { var w = x.label.split('/'); return w[1] + '/' + w[2] + '/' + w[0]; },
+            labels: ['Amount'],
+            lineColors: ['#a0d0e0'],
+            behaveLikeLine: true,
+            hideHover: 'auto',
+            smooth: false //No Curve
+        });
+    }
+
     initialize();
 })
