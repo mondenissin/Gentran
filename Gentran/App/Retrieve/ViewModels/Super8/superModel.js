fa243a4daceb5c1b1be6ee538a7ca07c992a5e22
gmcarreon <geraldcarreon24@gmail.com>
2016-11-18 11:01:38 +0800

    Nov. 18, 2016 - Updated

--- Gentran/App/Retrieve/ViewModels/Super8/superModel.js
+++ Gentran/App/Retrieve/ViewModels/Super8/superModel.js
@@ -194,6 +194,8 @@
                             $(elemName[0].element).remove();
                             //$scope.refreshSM();
                         }, execTime);
+
+                        viewModelHelper.saveTransaction(result.data.detail);
                     });
 
                     i++;
